f0cbaaaf84a747e3a9b3fc3b0a50a84a531e89c4
Shami1211 <it21203558@my.sliit.lk>
2024-04-10 20:53:32 +0530

    s

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,6 @@ import { Route, Routes } from "react-router";
 import AddItem from "./Components/Items/Admin/Add-Items/AddItem";
 import ItemDetails from "./Components/Items/Admin/Item/ItemDetails";
 import DashBoard from "./Components/Items/User/DashBoard/DashBoard";
-import BatDetails from "./Components/Items/User/DashBoard/BatDetails";
-import BallDetails from "./Components/Items/User/DashBoard/BallDetails";
-import GlovesDetails from "./Components/Items/User/DashBoard/GlovesDetails";
 
 import HelmetsDetails from "./Components/Items/User/DashBoard/HelmetsDetails";
 import PadsDetails from "./Components/Items/User/DashBoard/PadsDetails";
@@ -21,12 +18,7 @@ function App() {
           <Route path="/admin/additem" element={<AddItem />} />
           <Route path="/admin/itemdash" element={<ItemDetails />} />
           <Route path="/" element={<DashBoard />} />
-          <Route path="/batdetails" element={<BatDetails />} />
-          <Route path="/balldetails" element={<BallDetails />} />
-          <Route path="/glovesdetails" element={<GlovesDetails />} />
-          <Route path="/helmetdetails" element={<HelmetsDetails />} />
-          <Route path="/padsdetails" element={<PadsDetails />} />
-          <Route path="/shooesdetails" element={<ShoesDetails />} />
+   
           <Route path="/getitem" element={<GetItem />} />
         </Routes>
       </React.Fragment>
