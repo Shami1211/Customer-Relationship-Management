9611db1ceff05f075658f3e38755d9dcedd3c938
Shami1211 <it21203558@my.sliit.lk>
2024-04-10 14:24:10 +0530

    backend

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ItemRoute = require("./Routes/ItemRoute.js")
+const GetItemRoute = require("./Routes/GetItemRoute.js")
+const connectDB = require("./Config/db.js")
+const dotenv = require("dotenv")
+const cors = require("cors");
+const app = express();
+
+dotenv.config(); 
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use('/items',ItemRoute);
+app.use('/getitems',GetItemRoute);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+ 
